ac7d5df6538e2e14ce267ffb751c21ab15478de9
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-19 23:31:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/paramable'
 class Artist
-  extend Memorable, Findable
+  extend Memorable::ClassMethods, Findable
   include Paramable
   attr_accessor :name
   attr_reader :songs
