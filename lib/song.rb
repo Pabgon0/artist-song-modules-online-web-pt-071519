40513cbc3463cac0cae2a32b80976e5ca2818886
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-19 23:34:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,7 @@ require_relative '../lib/concerns/paramable'
 class Song
   extend Memorable, Findable
   include Paramable
-  attr_accessor :name
-  attr_reader :artist
+
   @@all = []
  
   def initialize
